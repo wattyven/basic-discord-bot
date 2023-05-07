7fc54ae5ab52ed75cf1999ee534280b2afa3d46c
Watty <66798673+wattyven@users.noreply.github.com>
2023-05-07 16:55:33 -0700

    Update bot.py
    key rework. new functionality

--- bot.py
+++ bot.py
@@ -2,54 +2,63 @@ import requests
 import discord
 from discord.ext import commands, pages
 from datetime import datetime
+import gpt4free
+from gpt4free import Provider
 
-# TO-DO: (probably at some point in the distant future when i get bored again or if i remember to come back to this)
-# help command + documentation for commands
-# command for getting most popular anime, as well as most popular anime by genre etc
-# improving search for multi-word / multi-term searches and better matching
-# switching to a better API (not super important as I'm the only one using this bot rn)
-# maybe user tracking for anime they've watched, anime they're currently watching, etc,
-# so eventually, users can get anime recommendations based on activity
-# also maybe social features like sharing anime with friends, etc
+with open("bot_keys.txt", "r") as f: # Opens the bot_keys.txt file and reads the keys.
+    keys = [i.strip() for i in f.readlines()] # removes the newline characters from the keys
+    print(keys)
 
+botID = keys[0] # discord ID of our bot
+botToken = keys[1] # discord bot token
+animeDBKey = keys[2] # API key for the Anime DB API
 
 intents = discord.Intents.default() # Sets the bot's intents to default.
 intents.message_content = True # Allows the bot to read messages.
 
 client = commands.Bot(command_prefix="$", intents=intents) # Sets the bot's prefix to "$" and intents to the intents variable.
 
-# bot = "###################"  # bot id, for personal reference
-
-Token = '---------------------------------------' # Replace with Discord Bot Token.
+Token = botToken 
        
 @client.event
 async def on_ready():
     print("Bot has started running") # Prints "Bot has started running" to the console when the bot is ready.
-    await client.change_presence(activity=discord.Game(name="prefix: $")) # Sets the bot's status to "prefix: $", 
-    # because there's no help command atm and people are bound to ask what the prefix is.
+    await client.change_presence(activity=discord.Game(name="prefix: $")) # Sets the bot's status to "prefix: $"
 
 @client.command()
 async def ping(ctx):
-    '''ping function to check if the bot is online'''
+    '''checks if the bot is online'''
     await ctx.send("Pong!")
 
 @client.command()
-async def search(ctx, search_term, page="1", size="50", genres="", sortBy="ranking", sortOrder="desc", types=""):
-    '''search function to search for anime using the API'''
+async def readme(ctx):
+    '''quick readme with commands. slightly neater than the default help command'''
+    embed = discord.Embed(title="Commands", description="Here are the commands for this bot", color=discord.Color.blue())
+    embed.add_field(name="$readme", value="Shows this message. Kinda redundant, huh.", inline=False)
+    embed.add_field(name="$ping", value="Checks if the bot is online", inline=False)
+    embed.add_field(name="$search", value="Searches for anime using the Anime DB API", inline=False)
+    await ctx.send(embed=embed)
+
+@client.command()
+async def search(ctx, *terms, page="1", size="50", genres="", sortBy="ranking", sortOrder="desc", types=""):
+    '''anime lookup using the Anime DB API'''
     url = "https://anime-db.p.rapidapi.com/anime" # API URL
     # this bot relies on Brian Rofiq's Anime DB API on RapidAPI, 
     # which can be found at https://rapidapi.com/brian.rofiq/api/anime-db/
 
+    search_term = " ".join(terms) # Converts the search terms to a string.
+    # if the search term is comprised of multiple words, add quotation marks around it
+    if len(terms) > 1:
+        search_term = "\"" + search_term + "\""
+
     headers = {
-        "X-RapidAPI-Key": "------------------------------------------------", # API key
+        "X-RapidAPI-Key": animeDBKey,
         "X-RapidAPI-Host": "anime-db.p.rapidapi.com"
     }
 
     querystring = {"page":page, "size":size, "search":search_term, "genres":genres, "sortBy":sortBy, "sortOrder":sortOrder, "types":types} # Query parameters
     # note: it looks like the sortBy and sortOrder parameters are not working as intended, 
     # so I guess we'll have to account for that in the code.
-
-    # did that below using a lambda function because i'm fucking lazy
      
     response = requests.get(url, headers=headers, params=querystring) # Sends a GET request to the API.
 
@@ -72,7 +81,57 @@ async def search(ctx, search_term, page="1", size="50", genres="", sortBy="ranki
     # sort our embeds by ranking, numerically, so that the highest ranking anime is first
     embeds.sort(key=lambda x: int(x.fields[1].value))
 
-    paginator = pages.Paginator(pages=embeds) # Creates a paginator object. https://docs.pycord.dev/en/stable/ext/pages/index.html#paginator
+    paginator = pages.Paginator(pages=embeds) # Creates a paginator object.
     await paginator.send(ctx) # Sends the paginator object to the user.
 
-client.run(Token) # Runs the bot with the token.
+@client.command()
+async def chat(ctx, *, message):
+    '''GPT-4 powered chat, powered by You through GPT4Free'''
+    # message = " ".join(text) # don't need this atm but I'll keep it here just in case
+    response = gpt4free.Completion.create(Provider.You, prompt=message)
+    await ctx.send(response)
+
+@client.command()
+async def search2(ctx, *, message):
+    '''anime search using the Anilist APIv2, powered by GraphQL'''
+    # define query as a multi-line string. You can also use triple quotes.
+    query = ''' 
+    query ($id: Int) { # Define which variables will be used in the query (id)
+    Media (id: $id, type: ANIME) { # Insert our variables into the query arguments (id) (type: ANIME is hard-coded in the query)
+        id
+        title {
+        romaji
+        english
+        native
+        }
+    }
+    }
+    '''
+
+    url = 'https://graphql.anilist.co'
+    try:
+        val = int(message.replace(" ", ""))
+        # i just realized this won't work because of titles like 86. gonna split this up into
+        # $search for search by title and $searchid for search by id. WIP
+    except ValueError:
+        val = message
+
+    if type(val) == int:
+        variables = {
+            'id': val
+        }
+        response = requests.post(url, json={'query': query, 'variables': variables})
+        # response is a json object, parse it for titles
+        response = response.json()
+        title_dict = response["data"]["Media"]["title"]
+        await ctx.send("Romaji: " + title_dict["romaji"] + "\nEnglish: " + title_dict["english"] + "\nNative: " + title_dict["native"])
+
+    elif type(val) == str:
+        variables = {
+            'search': val
+        }
+        response = requests.post(url, json={'query': query, 'variables': variables})
+    # input_type = type(val)
+    # await ctx.send(input_type)
+
+client.run(Token)
